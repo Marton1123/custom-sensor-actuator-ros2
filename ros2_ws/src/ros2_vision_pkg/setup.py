ee6f0f89387514784957b09d315fb73fe72f7bf2
Marton1123 <131730737+Marton1123@users.noreply.github.com>
2026-05-30 15:38:27 -0400

    fix: revertir copiado de modelos a ros2_vision_pkg y usar ruta relativa hacia IA/models en setup.py

--- ros2_ws/src/ros2_vision_pkg/setup.py
+++ ros2_ws/src/ros2_vision_pkg/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'models', 'botellas_vs_latas_ncnn'), glob('models/botellas_vs_latas_ncnn/*')),
+        (os.path.join('share', package_name, 'models', 'botellas_vs_latas_ncnn'), glob('../../../IA/models/botellas_vs_latas_ncnn/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
